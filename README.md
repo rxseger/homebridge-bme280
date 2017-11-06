03011fbbd7d1f9def177d5bc882d0099e147ba8d
NorthernMan54 <buyminivan-northern@yahoo.ca>
2017-11-05 22:53:42 -0500

    Add barometric pressure sensor support. Closes GH-4  (#2)
    * Added Pressure Sensor
    
    * Added Pressure Sensor
    
    * Supported negative temperatures
    
    * Added device polling and improved error handling
    
    * Added device polling and improved error handling
    
    * Added device polling and improved error handling
    
    * Added device polling and improved error handling
    
    * Fixed negative temperatures
    
    * $1
    
    * 1.1.1
    
    * added .gitignore
    
    * Added logging
    
    * Added logger
    
    * Added logging
    
    * Bugs
    
    * Bugs

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # homebridge-bme280
 
-[Bosch BME280](https://www.bosch-sensortec.com/bst/products/all_products/bme280) temperature/humidity sensor service plugin for [Homebridge](https://github.com/nfarina/homebridge)
+[Bosch BME280](https://www.bosch-sensortec.com/bst/products/all_products/bme280)
+temperature/humidity/barometric pressure sensor service plugin for [Homebridge](https://github.com/nfarina/homebridge)
 
 Uses [bme280-sensor](https://www.npmjs.com/package/bme280-sensor)
 
@@ -16,7 +17,9 @@ Connect the BME280 chip to the I2C bus
 * `name`: descriptive name
 * `name_temperature` (optional): descriptive name for the temperature sensor
 * `name_humidity` (optional): descriptive name for the humidity sensor
+* `refresh`: Optional, time interval for refreshing data in seconds, defaults to 60 seconds.
 * `options`: options for [bme280-sensor](https://www.npmjs.com/package/bme280-sensor)
+* `spreadsheetId` ( optional ): Log data to a google sheet, this is part of the URL of your spreadsheet.  ie the spreadsheet ID in the URL https://docs.google.com/spreadsheets/d/abc1234567/edit#gid=0 is "abc1234567".
 
 If you get an I/O error, make sure the I2C address is correct (usually 0x76 or 0x77 depending on a jumper).
 
@@ -39,13 +42,55 @@ Example configuration:
 
 This plugin creates two services: TemperatureSensor and HumiditySensor.
 
+## Optional - Enable access to Google Sheets to log data
+
+This presumes you already have a google account, and have access to google drive/sheets already
+
+Step 1: Turn on the Drive API
+a. Use this wizard ( https://console.developers.google.com/start/api?id=sheets.googleapis.com )
+to create or select a project in the Google Developers Console and automatically turn on the API. Click Continue, then Go to credentials.
+
+b. On the Add credentials to your project page, click the Cancel button.
+
+c. At the top of the page, select the OAuth consent screen tab. Select an Email address, enter a Product name if not already set, and click the Save button.  I used 'Sheets Data Logger'
+
+d. Select the Credentials tab, click the Create credentials button and select OAuth client ID.
+
+e. Select the application type Other, enter the name "Drive API Quickstart", and click the Create button.
+
+f. Click OK to dismiss the resulting dialog.
+
+g. Click the file_download (Download JSON) button to the right of the client ID.
+
+h. Move this file to your .homebridge and rename it logger_client_secret.json.
+
+Step 2: Authorize your computer to access your Drive Account
+
+a. Change to the directory where the plugin is installed i.e.
+
+cd /usr/lib/node_modules/homebridge-mcuiot/node_modules/mcuiot-logger
+
+b. Run the authorization module
+
+node quickstart.js
+
+c. Browse to the provided URL in your web browser.
+
+If you are not already logged into your Google account, you will be prompted to log in. If you are logged into multiple Google accounts, you will be asked to select one account to use for the authorization.
+
+d. Click the Accept button.
+
+e. Copy the code you're given, paste it into the command-line prompt, and press Enter.
+
 ## See also
 
 * [homebridge-ds18b20](https://www.npmjs.com/package/homebridge-ds18b20)
 * [homebridge-dht-sensor](https://www.npmjs.com/package/homebridge-dht-sensor)
 * [homebridge-dht](https://www.npmjs.com/package/homebridge-dht)
 
+## Credits
+* NorthernMan54 - Barometric Pressure and Device Polling
+
 ## License
 
 MIT
-
--- sensor.js
+++ sensor.js
@@ -3,100 +3,148 @@
 const bme280_sensor = require('bme280-sensor');
 
 let Service, Characteristic;
+var CommunityTypes;
+var debug = require('debug')('BME280');
+var logger = require("mcuiot-logger").logger;
 
 module.exports = (homebridge) => {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    CommunityTypes = require('hap-nodejs-community-types')(homebridge);
 
-  homebridge.registerAccessory('homebridge-bme280', 'BME280', BME280Plugin);
+    homebridge.registerAccessory('homebridge-bme280', 'BME280', BME280Plugin);
 };
 
-class BME280Plugin
-{
-  constructor(log, config) {
-    this.log = log;
-    this.name = config.name;
-    this.name_temperature = config.name_temperature || this.name;
-    this.name_humidity = config.name_humidity || this.name;
-    this.options = config.options || {};
+class BME280Plugin {
+    constructor(log, config) {
+        this.log = log;
+        this.name = config.name;
+        this.name_temperature = config.name_temperature || this.name;
+        this.name_humidity = config.name_humidity || this.name;
+        this.refresh = config['refresh'] || 60; // Update every minute
+        this.options = config.options || {};
+        this.spreadsheetId = config['spreadsheetId'];
+        if (this.spreadsheetId) {
+            this.log_event_counter = 59;
+            this.logger = new logger(this.spreadsheetId);
+        }
 
-    this.init = false;
-    this.data = {};
-    if ('i2cBusNo' in this.options) this.options.i2cBusNo = parseInt(this.options.i2cBusNo);
-    if ('i2cAddress' in this.options) this.options.i2cAddress = parseInt(this.options.i2cAddress);
-    console.log(`BME280 sensor options: ${JSON.stringify(this.options)}`);
-    this.sensor = new bme280_sensor(this.options);
-    this.sensor.init()
-      .then(result => {
-        console.log(`BME280 initialization succeeded`);
-        this.init = true;
-        this.readSensorData();
-    })
-    .catch(err => console.error(`BME280 initialization failed: ${err} `));
+        this.init = false;
+        this.data = {};
+        if ('i2cBusNo' in this.options) this.options.i2cBusNo = parseInt(this.options.i2cBusNo);
+        if ('i2cAddress' in this.options) this.options.i2cAddress = parseInt(this.options.i2cAddress);
+        this.log(`BME280 sensor options: ${JSON.stringify(this.options)}`);
 
-    this.temperatureService = new Service.TemperatureSensor(this.name_temperature);
+        try {
+            this.sensor = new bme280_sensor(this.options);
+        } catch (ex) {
+            this.log("BME280 initialization failed:", ex);
+        }
 
-    this.temperatureService
-      .getCharacteristic(Characteristic.CurrentTemperature)
-      .on('get', this.getCurrentTemperature.bind(this));
+        if (this.sensor)
+            this.sensor.init()
+            .then(result => {
+                this.log(`BME280 initialization succeeded`);
+                this.init = true;
 
-    this.humidityService = new Service.HumiditySensor(this.name_humidity);
-    this.humidityService
-      .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-      .on('get', this.getCurrentRelativeHumidity.bind(this));
-  }
-
-  // refresh sensor data
-  readSensorData() {
-    if (!this.init) return;
-    this.sensor.readSensorData()
-      .then(data => {
-        console.log(`data = ${JSON.stringify(data, null, 2)}`);
-        this.data = data;
-    })
-    .catch(err => console.log(`BME280 read error: ${err}`))
-  }
-
-  getCurrentTemperature(cb) {
-    this.sensor.readSensorData()
-      .then(data => {
-        console.log(`data(temp) = ${JSON.stringify(data, null, 2)}`);
-        cb(null, data.temperature_C);
-    })
-    .catch(err => {
-      console.log(`BME read error: ${err}`);
-      cb(err);
-    });
-  }
+                this.temperatureService
+                    .getCharacteristic(Characteristic.CurrentTemperature).getValue();
+            })
+            .catch(err => this.log(`BME280 initialization failed: ${err} `));
 
 
-  getCurrentRelativeHumidity(cb) {
-    this.sensor.readSensorData()
-      .then(data => {
-        console.log(`data(humi) = ${JSON.stringify(data, null, 2)}`);
-        cb(null, data.humidity);
-    })
-    .catch(err => {
-      console.log(`BME read error: ${err}`);
-      cb(err);
-    });
-  }
+        this.informationService = new Service.AccessoryInformation();
 
-  /* TODO
-  getCurrentPressure(cb) {
-    this.sensor.readSensorData()
-      .then(data => {
-        cb(null, data.pressure_hPA);
-    })
-    .catch(err => {
-      console.log(`BME read error: ${err}`);
-      cb(err);
-    });
-  }
-  */
+        this.informationService
+            .setCharacteristic(Characteristic.Manufacturer, "Bosch")
+            .setCharacteristic(Characteristic.Model, "RPI-BME280")
+            .setCharacteristic(Characteristic.SerialNumber, this.device);
 
-  getServices() {
-    return [this.temperatureService, this.humidityService]
-  }
+        this.temperatureService = new Service.TemperatureSensor(this.name_temperature);
+
+        this.temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .setProps({
+                minValue: -100,
+                maxValue: 100
+            })
+            .on('get', this.getCurrentTemperature.bind(this));
+
+        this.temperatureService
+            .addCharacteristic(CommunityTypes.AtmosphericPressureLevel);
+
+        this.humidityService = new Service.HumiditySensor(this.name_humidity);
+
+        setInterval(this.devicePolling.bind(this), this.refresh * 1000);
+
+    }
+
+    // refresh sensor data
+    readSensorData() {
+        if (!this.init) return;
+        this.sensor.readSensorData()
+            .then(data => {
+                this.log(`data = ${JSON.stringify(data, null, 2)}`);
+                this.data = data;
+            })
+            .catch(err => this.log(`BME280 read error: ${err}`))
+    }
+
+    getCurrentTemperature(cb) {
+        if (this.sensor) {
+            this.sensor.readSensorData()
+                .then(data => {
+                    this.log(`data(temp) = ${JSON.stringify(data, null, 2)}`);
+                    if (this.spreadsheetId) {
+                        this.log_event_counter = this.log_event_counter + 1;
+                        if (this.log_event_counter > 59) {
+                            this.logger.storeBME(this.name, 0, roundInt(data.temperature_C), roundInt(data.humidity), roundInt(data.pressure_hPa));
+                            this.log_event_counter = 0;
+                        }
+                    }
+                    this.temperatureService
+                        .setCharacteristic(CommunityTypes.AtmosphericPressureLevel, roundInt(data.pressure_hPa));
+                    this.humidityService
+                        .setCharacteristic(Characteristic.CurrentRelativeHumidity, roundInt(data.humidity));
+                    cb(null, roundInt(data.temperature_C));
+                })
+                .catch(err => {
+                    this.log(`BME read error: ${err}`);
+                    if (this.spreadsheetId) {
+                        this.logger.storeBME(this.name, 1, -999, -999, -999);
+                    }
+                    cb(err);
+                });
+        } else {
+            this.log("Error: BME280 Not Initalized");
+            cb(new Error("BME280 Not Initalized"));
+        }
+
+    }
+
+    getCurrentRelativeHumidity(cb) {
+        this.sensor.readSensorData()
+            .then(data => {
+                this.log(`data(humi) = ${JSON.stringify(data, null, 2)}`);
+                cb(null, data.humidity);
+            })
+            .catch(err => {
+                this.log(`BME read error: ${err}`);
+                cb(err);
+            });
+    }
+
+    devicePolling() {
+        debug("Polling BME280");
+        this.temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature).getValue();
+    }
+
+    getServices() {
+        return [this.informationService, this.temperatureService, this.humidityService]
+    }
 }
 
+function roundInt(string) {
+    return Math.round(parseFloat(string) * 10) / 10;
+}
